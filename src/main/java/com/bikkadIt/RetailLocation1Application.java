8a8a4ab38681fd198e5211dce296d8a829bbce3f
Saniya1926 <saniyamulla19@gmail.com>
2023-07-21 20:54:51 +0530

    This is a first commit

---
+++ src/main/java/com/bikkadIt/RetailLocation1Application.java
@@ -0,0 +1,13 @@
+package com.bikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailLocation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailLocation1Application.class, args);
+	}
+
+}
